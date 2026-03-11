3704204495b393ca893fbd624bd7c021a4e48edf
JohanHopital <135158274+JohanHopital@users.noreply.github.com>
2026-03-11 09:52:50 +0100

    Recette clean

--- src/main/java/Recette.java
+++ src/main/java/Recette.java
@@ -6,53 +6,62 @@ import java.util.HashSet;
 public class Recette {
 	String name;
 	String description;
-	String recette;
+	String steps;
 	String image;
-	HashMap<Ingredient, Float> quantities;
-	static HashSet<Recette> listeRecette = new HashSet<>();
+	HashMap<Ingredient, Integer> quantities;
+	static HashSet<Recette> recipeList = new HashSet<>();
 	
-	public Recette(String name, String description, String recette, String image, HashMap<Ingredient, Float> quantities) {
+	public Recette(String name, String description, String steps, String image, HashMap<Ingredient, Integer> quantities) {
 		super();
 		this.name = name;
 		this.description = description;
-		this.recette = recette;
+		this.steps = steps;
 		this.image = image;
 		this.quantities = quantities;
-		listeRecette.add(this);
-	}
-	
-	public static HashSet<Recette> getListeRecette() {
-		return listeRecette;
+		recipeList.add(this);
 	}
 
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getDescription() {
 		return description;
 	}
+	
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public String getRecette() {
-		return recette;
+	
+	public String getSteps() {
+		return steps;
 	}
-	public void setRecette(String recette) {
-		this.recette = recette;
-	}	
+	
+	public void setRecipe(String steps) {
+		this.steps = steps;
+	}
+	
 	public String getImage() {
 		return image;
 	}
+	
 	public void setImage(String image) {
 		this.image = image;
 	}
-	public HashMap<Ingredient, Float> getQuantities() {
+	
+	public HashMap<Ingredient, Integer> getQuantities() {
 		return quantities;
 	}
-	public void setQuantities(HashMap<Ingredient, Float> quantities) {
+	
+	public void setQuantities(HashMap<Ingredient, Integer> quantities) {
 		this.quantities = quantities;
+	}
+	
+	public static HashSet<Recette> getListeRecette() {
+		return recipeList;
 	}
 }
